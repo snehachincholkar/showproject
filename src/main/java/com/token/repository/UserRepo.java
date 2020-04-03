8ea970e9e1e164a4eb886dc4d3613444139f578d
snehachincholkar <63037067+snehachincholkar@users.noreply.github.com>
2020-04-03 12:58:34 +0530

    Update UserRepo.java

--- src/main/java/com/token/repository/UserRepo.java
+++ src/main/java/com/token/repository/UserRepo.java
@@ -11,10 +11,10 @@ import com.token.model.User;
 @Repository
 public interface UserRepo extends JpaRepository<User, Long> {
 	@Transactional
-	@Query(name = "findByToken", value = "select * from auth_user where token= :token", nativeQuery = true)
+	@Query(name = "findByToken", value = "select * from users where token= :token", nativeQuery = true)
 	public User findByToken( @Param(value = "token") String token);
 	
 	@Transactional
-	@Query(name = "findByCredentials", value = "select * from auth_user where mobile_number= :mobileNumber and password= :password", nativeQuery = true)
-	public User findByCredentials(@Param(value = "mobileNumber") String mobileNumber, @Param(value = "password") String password);
+	@Query(name = "findByCredentials", value = "select * from users where username= :username and password= :password", nativeQuery = true)
+	public User findByCredentials(@Param(value = "username") String username, @Param(value = "password") String password);
 }
